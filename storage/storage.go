0fc01b573ecefb3282f9a42b4f0c4d286aedbca3
Ceres Moon <84838324+WhenMoon-afk@users.noreply.github.com>
2026-04-05 07:35:23 -0700

    chore: remove Go scaffold

--- storage/storage.go
+++
@@ -1,6 +0,0 @@
-// Package storage handles JSON serialization of macros to/from disk.
-package storage
-
-// TODO: implement in Codex task
-// SaveMacro(path string, events []recorder.Event) error
-// LoadMacro(path string) ([]recorder.Event, error)
