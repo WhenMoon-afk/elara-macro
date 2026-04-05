465416f0a515915546d84787f22e9101c04aff36
Ceres Moon <84838324+WhenMoon-afk@users.noreply.github.com>
2026-04-05 06:43:37 -0700

    chore: remove Go scaffold

--- recorder/recorder.go
+++
@@ -1,8 +0,0 @@
-//go:build windows
-
-// Package recorder captures global mouse and keyboard events via Win32 low-level hooks.
-package recorder
-
-// TODO: implement in Codex task
-// SetWindowsHookEx(WH_KEYBOARD_LL), SetWindowsHookEx(WH_MOUSE_LL)
-// Store events as []Event{Type, X, Y, Key, TimestampMS}
